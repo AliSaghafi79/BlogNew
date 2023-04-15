e4a8aac850aca257b17176fb318fa9cb794ef28e
AliSaghafi79 <alisaghafi79@gmail.com>
2023-04-15 19:45:13 +0330

    changed Create.js and context,js

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const mycontext = createContext({
   data: [],
@@ -7,12 +8,30 @@ export const mycontext = createContext({
   steLoading: [],
   blog: [],
   setBlog: [],
+  title: [],
+  setTitle: [],
+  text: [],
+  setText: [],
 });
 
 const Contexts = ({ children }) => {
   const [data, setData] = useState([]);
   const [loading, steLoading] = useState(true);
   const [blog, setBlog] = useState([]);
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const Navigate = useNavigate();
+
+  const handleSubmit = () => {
+    const blog = { title, text };
+    fetch("http://localhost:8000/blogs/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    })
+      .then(setText(""), setTitle(""), alert("Successfuly"))
+      .then(Navigate("/"));
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -30,7 +49,18 @@ const Contexts = ({ children }) => {
 
   return (
     <mycontext.Provider
-      value={{ data , loading, steLoading, blog, setBlog }}
+      value={{
+        data,
+        loading,
+        steLoading,
+        blog,
+        setBlog,
+        title,
+        setTitle,
+        setText,
+        text,
+        handleSubmit,
+      }}
     >
       {children}
     </mycontext.Provider>
